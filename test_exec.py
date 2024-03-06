e9c2655ec2bb77bcd3c8d54a29dea489b8ab6ae5
Nikolai <nikdenof@gmail.com>
2024-03-06 21:16:50 +0100

    Tested imports, should work

--- test_exec.py
+++ test_exec.py
@@ -1,12 +1,13 @@
 import sys
 import torch
-import groundingdino
+from groundingdino.util.slconfig import SLConfig
 import cv2
 import numpy as np
+from GSAM.VISAM.util.box_ops import box_iou
 
 arr_img = np.array([[5, 8], [0, 255]], dtype=np.uint8)
 arr_img_resize = cv2.resize(arr_img, (300, 300))
-arr_img_load = cv2.imread("./data/raw/test1.jpg")
+# arr_img_load = cv2.imread("./data/raw/test1.jpg")
 # Get the default device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Default Device:", device)
